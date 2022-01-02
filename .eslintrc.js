3fdf206873a11c09375f403397afb060e020363b
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-01 18:55:37 -0600

    Eslint code reinforcement added

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'no-param-reassign': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-restricted-syntax': ['error', 'FunctionExpression', 'WithStatement', "BinaryExpression[operator='in']"],
+    'react/function-component-definition': 'off',
+  },
+  globals: {
+    React: 'readonly',
+    ReactDOM: 'readoonly',
+    document: 'readonly',
+  },
+};
