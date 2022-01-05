c0e9416e08fd5d523c15faea1c2bc8b163599c24
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-05 17:50:05 -0600

    semantic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import 'react-toastify/dist/ReactToastify.css'
+import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 
 ReactDOM.render(
