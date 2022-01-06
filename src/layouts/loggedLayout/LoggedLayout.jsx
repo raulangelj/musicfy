c04818c075b02ff0d7043e52ab9452e33a10f79c
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-05 19:01:00 -0600

    Routes added

--- src/layouts/loggedLayout/LoggedLayout.jsx
+++ src/layouts/loggedLayout/LoggedLayout.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import PropTypes from 'prop-types'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Grid, GridColumn, GridRow } from 'semantic-ui-react'
+import Routess from '../../routes/Routes'
 import './LoggedLayout.scss'
 
 const LoggedLayout = ({ user }) => {
@@ -9,22 +11,24 @@ const LoggedLayout = ({ user }) => {
     user: PropTypes.object.isRequired,
   }
   return (
-    <Grid className="logged-layout">
-      <Grid.Row>
-        <Grid.Column width={3}>
-          <h2>Menu left</h2>
-        </Grid.Column>
-        <Grid.Column className="content" width={13}>
-          <h2>Top BAr</h2>
-          <h2>Content</h2>
-        </Grid.Column>
-      </Grid.Row>
-      <GridRow>
-        <GridColumn width={16}>
-          <h2>Player</h2>
-        </GridColumn>
-      </GridRow>
-    </Grid>
+    <Router>
+      <Grid className="logged-layout">
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <h2>Menu left</h2>
+          </Grid.Column>
+          <Grid.Column className="content" width={13}>
+            <h2>Top BAr</h2>
+            <Routess />
+          </Grid.Column>
+        </Grid.Row>
+        <GridRow>
+          <GridColumn width={16}>
+            <h2>Player</h2>
+          </GridColumn>
+        </GridRow>
+      </Grid>
+    </Router>
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  const a = 0
+  return (
+    <div>
+      <h1>Home...</h1>
+    </div>
+  )
+}
+
+export default Home
