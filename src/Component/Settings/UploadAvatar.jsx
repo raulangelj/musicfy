0a0a9382d523cedc8f2c48ba3de09dc68a6eb4b0
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-07 15:33:27 -0600

    Drop zone for upload user avatar

--- src/Component/Settings/UploadAvatar.jsx
+++ src/Component/Settings/UploadAvatar.jsx
@@ -1,19 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import propTypes from 'prop-types'
-import { Image } from 'semantic-ui-react'
+import { Image, Input } from 'semantic-ui-react'
+import { useDropzone } from 'react-dropzone'
+import { toast } from 'react-toastify'
 import NoAvatar from '../../assets/png/user.png'
+import { auth, storage } from '../../firebase/firebaseConfig'
 
 const UploadAvatar = ({ user }) => {
   const [avatarUrl, setavatarUrl] = useState(user.photo)
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log('acceptedFiles: ', acceptedFiles)
+  })
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: 'image/jpeg, image/png',
+    noKeyboard: true,
+    onDrop,
+  })
 
   UploadAvatar.propTypes = {
     user: propTypes.object.isRequired,
   }
 
   return (
-    <div className="user-avatar">
-      <Image src={avatarUrl || NoAvatar} />
+    <div className="user-avatar" {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ? (
+          <Image src={NoAvatar} />
+        ) : (
+          <Image src={avatarUrl || NoAvatar} />
+        )
+      }
     </div>
   )
 }
