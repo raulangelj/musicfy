50adb2fd2f884a74e06aafc3fe29651761f7d36b
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-07 18:23:19 -0600

    Update user name functionality and styles

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,22 +1,35 @@
 /* eslint-disable no-unused-vars */
 import propTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
+import BasicModal from '../../Component/Modal/BasicModal'
 import UploadAvatar from '../../Component/Settings/UploadAvatar'
 import UserName from '../../Component/Settings/UserName'
 import './Settings.scss'
 
 const Settings = ({ user, setreloadApp }) => {
+  const [modal, setModal] = useState({
+    show: false,
+    title: '',
+    content: '',
+  })
+
   Settings.propTypes = {
     user: propTypes.object.isRequired,
     setreloadApp: propTypes.func.isRequired,
   }
+
   return (
     <div className="settings">
       <h1>Configuracion</h1>
       <div className="avatar-name">
         <UploadAvatar user={user} setreloadApp={setreloadApp} />
-        <UserName user={user} />
+        <UserName user={user} setModal={setModal} setreloadApp={setreloadApp} />
       </div>
+      <BasicModal show={modal.show} setmodal={setModal} title={modal.title}>
+        {
+          modal.content
+        }
+      </BasicModal>
     </div>
   )
 }
