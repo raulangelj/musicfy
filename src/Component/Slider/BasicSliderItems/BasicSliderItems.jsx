da22b2f699be98cf9e2578b3d2fbe43797d22e72
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-09 12:49:52 -0600

    item for artist slider

--- src/Component/Slider/BasicSliderItems/BasicSliderItems.jsx
+++ src/Component/Slider/BasicSliderItems/BasicSliderItems.jsx
@@ -1,9 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import propTypes from 'prop-types'
 import Slider from 'react-slick'
 import './BasicSliderItem.scss'
+import { storage } from '../../../firebase/firebaseConfig'
+import alertErrors from '../../../utils/AlertErros'
 
+// COMPONENT FOR ARTIST ITEM
+const RenderItem = ({ item }) => {
+  const [imageUrl, setImageUrl] = useState(null)
+  RenderItem.propTypes = {
+    item: propTypes.object.isRequired,
+  }
+
+  useEffect(() => {
+    storage.ref(`artist/${item.banner}`)
+      .getDownloadURL()
+      .then((res) => {
+        setImageUrl(res)
+      })
+      .catch((err) => {
+        alertErrors(err.code)
+      })
+  }, [])
+
+  return (
+    <div className="basic-slider-items__list-item">
+      <div
+        className="avatar"
+        style={{
+          backgroundImage: `url('${imageUrl}')`,
+        }}
+      />
+      <h3>{item.name}</h3>
+    </div>
+  )
+}
+
+// COMPONENT FOR SLIDER
 const BasicSliderItems = ({ title, data }) => {
   BasicSliderItems.propTypes = {
     title: propTypes.string.isRequired,
@@ -17,31 +51,18 @@ const BasicSliderItems = ({ title, data }) => {
     slidesToShow: 5,
     slidesToScroll: 1,
     centerMode: true,
-    className: 'basic-silder-items__list',
+    className: 'basic-slider-items__list',
   }
 
   return (
-    <div className="basic-slider-items">
+    <div className="basic-slider-items__list-items">
       <h2>{title}</h2>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {
+          data.map((artist) => (
+            <RenderItem key={artist.id} item={artist} />
+          ))
+        }
       </Slider>
     </div>
   )
