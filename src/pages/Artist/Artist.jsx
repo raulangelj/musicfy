894af761725f088806f00433597d136001594cd2
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-09 14:16:12 -0600

    Basic Artist page

--- src/pages/Artist/Artist.jsx
+++ src/pages/Artist/Artist.jsx
@@ -1,15 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import BannerArtist from '../../Component/Artists/BannerArtist'
+import { db } from '../../firebase/firebaseConfig'
+import alertErrors from '../../utils/AlertErros'
 import './Artist.scss'
 
 const Artist = () => {
   const params = useParams()
-  console.log('params: ', params)
+  const { id = '' } = params
+  const [artist, setArtist] = useState(null)
+
+  useEffect(() => {
+    db.collection('artists')
+      .doc(id)
+      .get()
+      .then((res) => {
+        setArtist(res.data())
+      })
+      .catch((err) => {
+        alertErrors(err.code)
+      })
+  }, [id])
 
   return (
     <div>
-      <h2>artist page</h2>
+      {
+        artist && <BannerArtist artist={artist} />
+      }
     </div>
   )
 }
