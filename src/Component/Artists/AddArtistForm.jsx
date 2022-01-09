e16feb5fb413f40ab1b3b2a5cafbf3092da006c4
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-09 09:23:53 -0600

    modal/pop up add artist done

--- src/Component/Artists/AddArtistForm.jsx
+++ src/Component/Artists/AddArtistForm.jsx
@@ -5,12 +5,21 @@ import { useDropzone } from 'react-dropzone'
 import {
   Button, Form, Input, Image, FormField,
 } from 'semantic-ui-react'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
 import NoImage from '../../assets/png/no-image.png'
 import './AddArtistForm.scss'
+import { db, storage } from '../../firebase/firebaseConfig'
+
+const initialValueForm = () => ({
+  name: '',
+})
 
 const AddArtistForm = ({ setModal }) => {
   const [banner, setBanner] = useState(null)
   const [file, setFile] = useState(null)
+  const [formData, setFormData] = useState(initialValueForm)
+  const [isloading, setIsloading] = useState(false)
 
   AddArtistForm.propTypes = {
     setModal: propTypes.func.isRequired,
@@ -28,12 +37,59 @@ const AddArtistForm = ({ setModal }) => {
     onDrop,
   })
 
+  const uploadImage = (fileName) => {
+    const ref = storage
+      .ref()
+      .child(`artist/${fileName}`)
+
+    return ref.put(file)
+  }
+
+  const resetForm = () => {
+    setFormData(initialValueForm())
+    setFile(null)
+    setBanner(null)
+  }
+
   const onSubmit = () => {
-    console.log('creando artist')
-    setModal((prevState) => ({
-      ...prevState,
-      show: false,
-    }))
+    // setIsloading(false)
+    // setModal((prevState) => ({
+    //   ...prevState,
+    //   show: false,
+    // }))
+    if (!formData.name) {
+      toast.warning('Agrega el nombre del artista!')
+    } else if (!file) {
+      toast.warning('Agrega la imagen del artista!')
+    } else {
+      setIsloading(true)
+      const fileName = uuidv4()
+      uploadImage(fileName)
+        .then((res) => {
+          db.collection('artists')
+            .add({
+              name: formData.name,
+              banner: fileName,
+            })
+            .then(() => {
+              toast.success('Artista creado correctamente!')
+              resetForm()
+              setIsloading(false)
+              setModal((prevState) => ({
+                ...prevState,
+                show: false,
+              }))
+            })
+            .catch(() => {
+              toast.error('Error al crear el artista, intento mas tarde.')
+              setIsloading(false)
+            })
+        })
+        .catch((err) => {
+          toast.error('Error al subir la imagen, intentalo mas tarde.')
+          setIsloading(false)
+        })
+    }
   }
 
   return (
@@ -46,7 +102,7 @@ const AddArtistForm = ({ setModal }) => {
             backgroundImage: `url('${banner}')`,
           }}
         />
-        <Input {...getInputProps()} />
+        <input {...getInputProps()} />
         {!banner && <Image src={NoImage} />}
       </FormField>
       <FormField className="artist-avatar">
@@ -60,9 +116,10 @@ const AddArtistForm = ({ setModal }) => {
       <FormField>
         <Input
           placeholder="Nombre del artista"
+          onChange={(e) => setFormData({ name: e.target.value })}
         />
       </FormField>
-      <Button type="submit">
+      <Button type="submit" loading={isloading}>
         Crear Artista
       </Button>
     </Form>
