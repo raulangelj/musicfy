39d214ebd778dcb11041055aea923a6429d98d40
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-10 13:35:15 -0600

    Routes for Artist Page

---
+++ src/pages/Artists/Artists.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Artists.scss'
+
+const Artists = () => {
+  const a = 0
+  return (
+    <div className="artists">
+      <h1>Artistas</h1>
+    </div>
+  )
+}
+
+export default Artists
