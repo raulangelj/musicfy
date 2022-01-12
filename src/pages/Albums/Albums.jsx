971d0e99c6d2988a4435890036beba7da625e765
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-11 20:17:55 -0600

    Albums page added to routes

---
+++ src/pages/Albums/Albums.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Albums.scss'
+
+const Albums = () => {
+  const a = 0
+  return (
+    <div>
+      <h2>Albums ..</h2>
+    </div>
+  )
+}
+
+export default Albums
