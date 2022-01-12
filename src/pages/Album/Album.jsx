ea91faeccf353190838fc7508dbe912207118087
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-11 21:04:21 -0600

    Getting data for artist, album and image from firebase

--- src/pages/Album/Album.jsx
+++ src/pages/Album/Album.jsx
@@ -1,11 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { db, storage } from '../../firebase/firebaseConfig'
+import alertErrors from '../../utils/AlertErros'
 import './Album.scss'
 
 const Album = () => {
   const params = useParams()
-  console.log('params: ', params)
+  const { id } = params
+  const [album, setAlbum] = useState(null)
+  const [albumImage, setAlbumImage] = useState(null)
+  const [artist, setArtist] = useState(null)
+
+  // USEEFECT TO GET ALBUM IMAGE IN FIRESTORE
+  useEffect(() => {
+    db.collection('albums')
+      .doc(id)
+      .get()
+      .then((res) => {
+        setAlbum(res.data())
+      })
+      .catch((err) => {
+        alertErrors(err)
+      })
+  }, [id])
+
+  // USEEFECT TO GET ALBUM IMAGE IN STOREGE
+  useEffect(() => {
+    if (album) {
+      storage.ref(`albums/${album?.banner}`)
+        .getDownloadURL()
+        .then((res) => {
+          setAlbumImage(res)
+        })
+        .catch((err) => {
+          alertErrors(err.code)
+        })
+    }
+  }, [album])
+
+  // USEEFECT TO GET ARTIST INFO IN FIRESTORE
+  useEffect(() => {
+    if (album) {
+      db.collection('artists')
+        .doc(album?.artist)
+        .get()
+        .then((res) => {
+          setArtist(res.data())
+        })
+        .catch((err) => {
+          alertErrors(err.code)
+        })
+    }
+  }, [album])
 
   return (
     <div>
