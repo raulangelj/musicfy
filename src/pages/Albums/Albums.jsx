dd8d0bcda8700db2a5e75fe4a8488832f9b0515e
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-11 20:26:41 -0600

    get data form firebase

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -1,12 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../firebase/firebaseConfig'
+import alertErrors from '../../utils/AlertErros'
 import './Albums.scss'
 
 const Albums = () => {
-  const a = 0
+  const [albums, setAlbums] = useState([])
+  console.log('albums: ', albums)
+
+  useEffect(() => {
+    db.collection('albums')
+      .get()
+      .then((res) => {
+        const arrayAlbums = []
+        res.docs?.forEach((doc) => {
+          const data = doc.data()
+          data.id = doc.id
+          arrayAlbums.push(data)
+        })
+        setAlbums(arrayAlbums)
+      })
+      .catch((err) => {
+        alertErrors(err.code)
+      })
+  }, [])
+
   return (
-    <div>
-      <h2>Albums ..</h2>
+    <div className="albums">
+      <h1>Albumes</h1>
     </div>
   )
 }
