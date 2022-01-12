c0637762745a2bfc4dc4323ac2594b5ee4cd8939
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-01-11 20:41:34 -0600

    All albums page done

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -1,12 +1,47 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
-import { db } from '../../firebase/firebaseConfig'
+import { Grid, GridColumn } from 'semantic-ui-react'
+import propTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { db, storage } from '../../firebase/firebaseConfig'
 import alertErrors from '../../utils/AlertErros'
 import './Albums.scss'
 
+const Album = ({ album }) => {
+  const [imageUrl, setImageUrl] = useState(null)
+
+  Album.propTypes = {
+    album: propTypes.object.isRequired,
+  }
+
+  useEffect(() => {
+    storage.ref(`albums/${album.banner}`)
+      .getDownloadURL()
+      .then((res) => {
+        setImageUrl(res)
+      })
+      .catch((err) => {
+        alertErrors(err.code, ' de albumes')
+      })
+  }, [album])
+
+  return (
+    <Link to={`/album/${album.id}`}>
+      <div className="albums__item">
+        <div
+          className="avatar"
+          style={{
+            backgroundImage: `url('${imageUrl}')`,
+          }}
+        />
+        <h3>{album.name}</h3>
+      </div>
+    </Link>
+  )
+}
+
 const Albums = () => {
   const [albums, setAlbums] = useState([])
-  console.log('albums: ', albums)
 
   useEffect(() => {
     db.collection('albums')
@@ -28,6 +63,15 @@ const Albums = () => {
   return (
     <div className="albums">
       <h1>Albumes</h1>
+      <Grid>
+        {
+          albums?.map((album) => (
+            <GridColumn key={album.id} mobile={8} tablet={4} computer={3}>
+              <Album album={album} />
+            </GridColumn>
+          ))
+        }
+      </Grid>
     </div>
   )
 }
