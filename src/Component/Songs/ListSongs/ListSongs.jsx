340d44796b40531d607043d7e07c4e4e3e858d33
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-02-04 22:22:39 -0600

    table first styles

--- src/Component/Songs/ListSongs/ListSongs.jsx
+++ src/Component/Songs/ListSongs/ListSongs.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import propType from 'prop-types'
+import {
+  Table, Icon, TableHeader, TableRow, TableHeaderCell, TableBody, TableCell,
+} from 'semantic-ui-react'
 import './ListSongs.scss'
 
 const ListSongs = ({ songs, albumImg }) => {
@@ -10,9 +13,36 @@ const ListSongs = ({ songs, albumImg }) => {
   }
 
   return (
-    <div>
-      <h1>List Songs...</h1>
-    </div>
+    <Table inverted className="list-songs">
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell />
+          <TableHeaderCell>Titulo</TableHeaderCell>
+          {/* <TableHeaderCell>Album</TableHeaderCell> */}
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        <TableRow>
+          <TableCell collapsing>
+            <Icon name="play circle outline" />
+          </TableCell>
+          <TableCell>cancion 1</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell collapsing>
+            <Icon name="play circle outline" />
+          </TableCell>
+          <TableCell>cancion 2</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell collapsing>
+            <Icon name="play circle outline" />
+          </TableCell>
+          <TableCell>cancion 3</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   )
 }
 
